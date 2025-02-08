f86c3132394e9d591c6d8e18fed919a2d4867e5d
Seryy-code <62565090+Seryy-code@users.noreply.github.com>
2025-02-08 14:33:32 +0100

    Routes and Controllers

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello world!");
-});
+// app.get("/", (req, res) => {
+//   res.send("hello world!");
+// });
+
+// app.get("/task", (req, res) => {
+//   res.send("tasks for cars");
+// });
+
+const postsRoute = require("./routes/tasks");
+app.use("/tasks", postsRoute);
 
 module.exports = app;
