3a4a744048220f6befab86a3c735cdbf910dc49d
Seryy-code <62565090+Seryy-code@users.noreply.github.com>
2025-03-12 10:39:23 +0100

    edit and add methods POST, DELETE, UPDATE

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -2,6 +2,10 @@ const express = require("express");
 const taskController = require("../controllers/task.controller");
 const router = express.Router();
 
-router.get("/task", taskController.save);
+router.post("/task", taskController.save);
+router.get("/task/:id", taskController.show);
+router.get("/task", taskController.index);
+router.patch("/task/:id", taskController.update);
+router.delete("/task/:id", taskController.destroy);
 
 module.exports = router;
